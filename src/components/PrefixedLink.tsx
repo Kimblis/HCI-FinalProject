6932d002166f6964adfa2c57b734ad00eb69f0cb
Kimblis <e.masilionis122@gmail.com>
2023-12-12 18:58:50 +0700

    solution

--- src/components/PrefixedLink.tsx
+++ src/components/PrefixedLink.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const PrefixedLink = ({ href, as = href, children, ...props }: {href: string, as?: string, children?: any, props?: any}) => (
   <Link
-    color={'teal.500'} 
+    style={{color: '#319795', paddingLeft: '10px' }}
     href={href}
     as={`${process.env.PROJECT_PREFIX}${as}`}
     {...props}
